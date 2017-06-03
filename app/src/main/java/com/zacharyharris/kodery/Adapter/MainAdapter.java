798bb354c2ee4c7d1add412f4878b8436e309653
Alex Lue <alue@ucsc.edu>
2017-06-03 02:25:33 -0700

    Added Long click listener for boards
    two different handlers for long click:
    popup or switch activities

--- app/src/main/java/com/zacharyharris/kodery/Adapter/MainAdapter.java
+++ app/src/main/java/com/zacharyharris/kodery/Adapter/MainAdapter.java
@@ -1,16 +1,20 @@
 package com.zacharyharris.kodery.Adapter;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zacharyharris.kodery.Model.Board;
 import com.zacharyharris.kodery.R;
+import com.zacharyharris.kodery.UI.MainActivity;
 import com.zacharyharris.kodery.UI.SingleBoardActivity;
 
 import java.util.ArrayList;
@@ -20,9 +24,12 @@ import static com.zacharyharris.kodery.R.id.board;
 /**
  * Created by AlexLue on 5/24/17.
  */
+/*
+
+CURRENTLY NOT IN USE. TRANSFERRED TO MAIN ACTIVITY
 
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
-    
+
     public static final String TAG = "MainAdapter";
     private ArrayList<Board> boardsList;
 
@@ -50,7 +57,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
         return boardsList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,View.OnLongClickListener{
 
         public TextView mBoard;
         public int position;
@@ -58,6 +65,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
         public ViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
             mBoard =(TextView) itemView.findViewById(board);
         }
 
@@ -71,5 +79,41 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
             v.getContext().startActivity(i);
 
         }
+
+        @Override
+        public boolean onLongClick(View v) {
+            Toast.makeText(v.getContext(), "LONG Click",Toast.LENGTH_SHORT).show();
+
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(v.getContext());
+            View mview = LayoutInflater.from(v.getContext()).inflate(R.layout.edit_board, null);
+            final EditText mboardname = (EditText) mview.findViewById(R.id.boardnme_edit);
+            Button saveleboard = (Button) mview.findViewById(R.id.saveBoard);
+            Button delboard = (Button) mview.findViewById(R.id.delBoard);
+
+            saveleboard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!mboardname.getText().toString().isEmpty()){
+                        Toast.makeText(v.getContext(),
+                                "Task Created!",
+                                Toast.LENGTH_SHORT).show();
+                        // Get rid of the pop up go back to main activity
+                    }else{
+                        Toast.makeText(v.getContext(),
+                                "Please name the task.",
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+            mBuilder.setView(mview);
+            AlertDialog dialog = mBuilder.create();
+            dialog.show();
+
+            return true;
+        }
     }
 }
+
+*/
