11b0e6ac43eed86ff90a53b31c3581c3a0078ade
Alex Lue <alue@ucsc.edu>
2017-06-09 02:58:16 -0700

    Added a Load up page
    Temporary, may change if necessary

---
+++ app/src/main/java/com/zacharyharris/kodery/UI/LoadActivity.java
@@ -0,0 +1,39 @@
+package com.zacharyharris.kodery.UI;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zacharyharris.kodery.R;
+
+public class LoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+        android.support.v7.app.ActionBar menu = getSupportActionBar();
+        menu.hide();
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(2000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(LoadActivity.this,
+                            MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+
+    }
+}
